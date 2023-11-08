7e6a1d960e0a06460543027e853211481b22f3f5
Guvatara <guvatara@gmail.com>
2023-11-08 21:23:38 +0300

    fix MUI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,11 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Link from '@/components/Link';
 import ProTip from '@/components/ProTip';
 import Copyright from '@/components/Copyright';
 
-export default function About() {
+export default function Home() {
     return (
         <Container maxWidth="lg">
             <Box
@@ -22,11 +21,9 @@ export default function About() {
                 <Typography variant="h4" component="h1" gutterBottom>
                     Material UI - Next.js example in TypeScript
                 </Typography>
-                <Box sx={{ maxWidth: 'sm' }}>
-                    <Button variant="contained" component={Link} noLinkStyle href="/">
-                        Go to the home page
-                    </Button>
-                </Box>
+                <Link href="/about" color="secondary">
+                    Go to the about page
+                </Link>
                 <ProTip />
                 <Copyright />
             </Box>
