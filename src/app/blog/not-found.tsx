beba4096aa464036ed4a8784882caf5ba6b00540
Guvatara <guvatara@gmail.com>
2023-11-09 19:02:24 +0300

    add linter

--- src/app/blog/not-found.tsx
+++ src/app/blog/not-found.tsx
@@ -1,11 +1,12 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function NotFound() {
-    return (
-        <div>
-            <h2>Not Found</h2>
-            <p>Could not find requested resource</p>
-            <Link href="/">Return Home</Link>
-        </div>
-    )
+  return (
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
 }
