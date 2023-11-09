8e715affc84081278f905f79c3259e5b8deb5da7
Guvatara <guvatara@gmail.com>
2023-11-09 21:35:14 +0300

    add theme colors

--- src/components/ThemeRegistry/theme.ts
+++ src/components/ThemeRegistry/theme.ts
@@ -8,38 +8,41 @@ const roboto = Roboto({
   display: "swap",
 });
 
+const colors = {
+  blue: "#2575dc",
+  indigo: "#6610f2",
+  purple: "#5d54e4",
+  pink: "#e83e8c",
+  red: "#e45454",
+  orange: "#fb9c5d",
+  yellow: "#ffc107",
+  green: "#7fad4a",
+  teal: "#20c997",
+  cyan: "#17a2b8",
+  white: "#fff",
+  gray: "#6c757d",
+  grayDark: "#343a40",
+  success: "#7fad4a",
+  info: "#52baff",
+  warning: "#fb9c5d",
+  danger: "#e45454",
+  light: "#ebeaec",
+  dark: "#2a2b2d",
+  primary: "#2575dc",
+  secondary: "#67646b",
+};
+
 const theme = createTheme({
   palette: {
     primary: {
       main: "#556cd6",
-      contrast: "#2575dc",
     },
     secondary: {
       main: "#19857b",
-      contrast: "#67646b",
     },
     error: {
       main: red.A400,
     },
-    blue: { main: "#2575dc" },
-    indigo: { main: "#6610f2" },
-    purple: { main: "#5d54e4" },
-    pink: { main: "#e83e8c" },
-    red: { main: "#e45454" },
-    orange: { main: "#fb9c5d" },
-    yellow: { main: "#ffc107" },
-    green: { main: "#7fad4a" },
-    teal: { main: "#20c997" },
-    cyan: { main: "#17a2b8" },
-    white: { main: "#fff" },
-    gray: { main: "#6c757d" },
-    grayDark: { main: "#343a40" },
-    success: { main: "#7fad4a" },
-    info: { main: "#52baff" },
-    warning: { main: "#fb9c5d" },
-    danger: { main: "#e45454" },
-    light: { main: "#ebeaec" },
-    dark: { main: "#2a2b2d" },
   },
   typography: {
     fontFamily: roboto.style.fontFamily,
@@ -57,7 +60,7 @@ const theme = createTheme({
     MuiAppBar: {
       styleOverrides: {
         root: {
-          backgroundColor: "#fafafa",
+          backgroundColor: colors.light,
         },
       },
     },
